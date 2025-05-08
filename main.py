33d55a7a8d59fa8f729916245c421248ae0c036f
AndreyKhodulapov <andrey.corsar1993@gmail.com>
2025-05-08 10:47:10 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from router import router as book_router
+
+
+app = FastAPI()
+app.include_router(book_router)
+
+@app.get("/")
+async def root():
+    return HTMLResponse(
+        """
+        <head>
+        <title>
+        my little library app
+        </title>
+        </head>
+        <body>
+        <h1 style="font-size: 50px; text-align: center">
+        WELCOME TO MY LIBRARY!
+        </h1>
+        </body>
+        """
+    )
